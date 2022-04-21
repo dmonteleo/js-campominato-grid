e1b10676f33d5a0dba09ead7388d34b2862d70b5
dmonteleo <dmonteleo@gmail.com>
2022-04-21 20:39:04 +0200

    grid 100

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,25 @@
 const listNumbers = [];
+const griglia = document.querySelector('.griglia');
+
+init();
 
 function createSquare(target) {
   const square = document.createElement('div');
   square.className = 'square';
   const number = getUniqueRandomNumber(1, 100);
-  square.innerHTML = `<span>${muber}</span>`;
+  square.innerHTML = `<span>${number}</span>`;
   square.classList.add(getOddEven(number));
   target.append(square);
   return square;
+}
+
+function init() {
+  for(i=1; i<=100; i++) {
+    const square = createSquare(griglia);
+    square.addEventListener('click', function() {
+      this.classList.add('clicked');
+    })
+  }
 }
 
 function getUniqueRandomNumber(min, max) {
@@ -27,3 +39,7 @@ function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function getOddEven(n) {
+  if(n % 2) return 'odd';
+  return event;
+}
